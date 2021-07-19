46e488ffce3ebeec70d6ae0713d23f5b6d725d1d
prncn <princen@outlook.de>
2021-07-19 20:57:56 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,89 @@
+const clientId = "b3476355e8434cdea6c69faf524c8db3"
+const clientSecret = "e349effa6ee3489ba63c56ab59cae285"
+const playlist = "2b7qybyNGcVXVb5kVsEfBX"
+
+async function get_token() {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+    },
+    body: 'grant_type=client_credentials'
+  }).catch(error => console.log(error));
+
+  const data = await result.json();
+  return data.access_token;
+}
+
+async function get_items(offset) {
+  const result = await fetch(`https://api.spotify.com/v1/playlists/${playlist}/tracks?offset=${offset}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + await get_token()
+    }
+  }).catch(error => console.log(error));
+
+  const data = await result.json();
+  return data.items;
+}
+
+async function solve_contributors() {
+  let contribs = []
+  let dataset = {}
+  
+  for(let i=0; i<5; i++) {
+    items = await get_items(i*100);
+    for(item of items) {
+      contribs.push(item.added_by.id)
+    }
+  }
+
+  console.log(contribs);
+
+  for(contrib of contribs) {
+    if(await contrib in dataset) {
+      dataset[contrib]++;
+    } else {
+      dataset[contrib] = 0;
+    }
+  }
+
+  console.log(dataset);
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: Object.keys(dataset),
+      datasets: [{
+        data: Object.values(dataset),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 72, 54, 0.2)'
+        ]
+      }]
+    },
+  });
+
+  return dataset;
+}
+
+solve_contributors();
+
+
+function count(list, key) {
+  for(item of list){
+    if(item === key){
+      count++;
+    }
+  }
+  return count;
+}
+
+
